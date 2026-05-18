f99519f0422c422683db1854b217b144223c2edb
Omar-Khaled-57 <khaledelkhly57@gmail.com>
2026-05-18 14:27:40 +0300

    c'mon now

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -238,6 +238,8 @@ const Home = () => {
                   }`}>
                     <video
                       src="Animation1.webm"
+                      preload="auto"
+                      crossOrigin="anonymous"
                       autoPlay
                       loop
                       muted
